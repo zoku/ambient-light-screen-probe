dcbe24bfe1bf06d74a00f9d03829951020e7f8d6
zoku <mail@zoku.io>
2023-08-11 12:41:44 +0200

    Correct ProbePoints class

--- ScreenColorProbes/ProbePoints.cs
+++ ScreenColorProbes/ProbePoints.cs
@@ -2,26 +2,23 @@
 
 namespace ScreenColorGrabber
 {
-    public partial class MainWindow
-    {
-        private class ProbePoints{
-            List<int> x { get; set; }
-            List<int> y { get; set; }
+    public class ProbePoints{
+        List<int> x { get; set; }
+        List<int> y { get; set; }
 
-            int top { get; set; }
-            int right { get; set; }
-            int bottom { get; set; }
-            int left { get; set; }
+        int top { get; set; }
+        int right { get; set; }
+        int bottom { get; set; }
+        int left { get; set; }
 
-            public ProbePoints(List<int> x, List<int> y, int top, int right, int bottom, int left)
-            {
-                this.x = x;
-                this.y = y;
-                this.top = top;
-                this.right = right;
-                this.bottom = bottom;
-                this.left = left;
-            }
+        public ProbePoints(List<int> x, List<int> y, int top, int right, int bottom, int left)
+        {
+            this.x = x;
+            this.y = y;
+            this.top = top;
+            this.right = right;
+            this.bottom = bottom;
+            this.left = left;
         }
     }
 }
